63d22de2d6719ddfa28ef1e7b1d337cdd7d70581
Vicktor Juhel <98396727+VJuel@users.noreply.github.com>
2024-09-25 09:31:50 +0200

    github pages setup deploy nuxt project test domain Google Auth

--- env.d.ts
+++ env.d.ts
@@ -3,9 +3,10 @@
 API_GOOGLE = "AIzaSyB-qSq-kF95pZCTY7cPzlLjiqUAl9-Ki24;";
 YOUR_CLIENT_ID = "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";
 YOUR_CLIENT_SECRET = "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";
-YOUR_REDIRECT_URL = " https://localhost:3000/oauth2callback";
+YOUR_REDIRECT_URL =
+  " https://vjuel.github.io/Google-Docs-Clone/dist/oauth2callback";
 ID_TEMPLATE = "1YPNdcIs48voNPoDq0eHzefR1_LCsXGUDj87rBZaJW98";
-
+NUXT_APP_BASE_URL = "https://VJuel.github.io/Google-Docs-Clone/";
 // https://developers.google.com/docs/api/reference/rest/v1/documents?hl=fr
 
 // https://googleapis.dev/nodejs/googleapis/latest/docs/index.html#google-apis
